e15852e1c31e38e508e7ee3def433529ff40b98f
MyoThiriZin <myothirizin99@gmail.com>
2023-06-22 12:00:52 +0630

    upload project

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <!-- Begin page -->
+  <div id="layout-wrapper" :class="{'right-bar-enabled':show, 'sidebar-enable':showL}">
+    <HeaderTop @showMenuRight="showMenuRight" @handleLeft="showMenuLeft"/>
+
+    <!-- ========== Left Sidebar Start ========== -->
+    <LeftSidebar />
+    <!-- Left Sidebar End -->
+
+    <!-- Start right Content here -->
+    <MainContent />
+    <!-- end main content-->
+  
+  <!-- END layout-wrapper -->
+
+  <!-- Right Sidebar -->
+  <RightBar @close="show=false" />
+  <!-- /Right-bar -->
+
+  <!-- Right bar overlay-->
+  <div class="rightbar-overlay" @click="show=false"></div>
+</div>
+</template>
+
+<script>
+// import $ from 'jquery'
+
+import HeaderTop from "./components/HeaderTop.vue";
+import LeftSidebar from "./components/LeftSidebar.vue";
+import MainContent from "./components/MainContent.vue";
+import RightBar from "./components/RightBar.vue";
+
+export default {
+
+  name: "App",
+  components: {
+    HeaderTop,
+    LeftSidebar,
+    MainContent,
+    RightBar,
+  },
+    data() {
+    return{
+      show: false,
+      showL: false
+    }
+  },
+  methods: {
+    
+    showMenuRight() {
+      this.show = !this.show;
+    },
+    showMenuLeft() {
+      this.toggleSize();
+      this.showL = !this.showL;
+      
+    },
+    toggleSize() {
+            const currentSidebarSize = document.body.getAttribute('data-sidebar-size');
+
+      if (window.innerWidth >= 992) {
+        if (currentSidebarSize === null || currentSidebarSize === 'lg') {
+          document.body.setAttribute('data-sidebar-size', 'sm');
+        } else if (currentSidebarSize === 'md') {
+          document.body.setAttribute('data-sidebar-size', 'sm');
+        } else {
+          document.body.setAttribute('data-sidebar-size', 'lg');
+        }
+      }
+    }
+  },
+  mounted() {
+    
+  }
+};
+</script>
+
+<style>
+@import "../public/assets/css/bootstrap.min.css";
+@import "../public/assets/css/icons.min.css";
+@import "../public/assets/css/app.min.css";
+</style>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+
+// import Vue from 'vue'
+// import App from './App.vue'
+
+
+// import './assets/libs/bootstrap/js/bootstrap.bundle.min.js'
+// import './assets/libs/metismenu/metisMenu.min.js'
+// import './assets/libs/simplebar/simplebar.min.js'
+// import './assets/libs/node-waves/waves.min.js'
+// import './assets/libs/waypoints/lib/jquery.waypoints.min.js'
+// import './assets/libs/jquery.counterup/jquery.counterup.min.js'
+// import './assets/libs/apexcharts/apexcharts.min.js'
+// import './assets/js/pages/dashboard.init'
+// import './assets/js/app'
+// Vue.config.productionTip = false;
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app');
+
+import { createApp } from 'vue'
+// import Vue from 'vue';
+import App from './App.vue'
+// import VueApexCharts from "vue3-apexcharts";
+
+
+createApp(App).mount('#app')
+
